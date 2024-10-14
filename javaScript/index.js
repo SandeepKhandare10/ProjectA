998cc1fa38aa1aa030c42425d01ca2ca688c7052
SandeepKhandare10 <sandipkhandare10@gmail.com>
2024-10-14 17:25:27 +0530

    css add

---
+++ javaScript/index.js
@@ -0,0 +1,31 @@
+// alert("ok")
+// console.log("hello i am console")
+// document.write("I am document")
+document.getElementById("one").innerHTML="Hello world";
+// {
+//     var x=58;
+// }
+// // var x=30;
+// x=56;
+// console.log(x);
+
+// {
+//     let s=47;
+//     console.log(s)
+// }
+// {
+//     const c=67;
+//     console.log(c);
+// }
+
+
+g=39;
+var g;
+console.log(g);
+
+v=37;
+var v;
+
+{ 
+    console.log(v)
+}
